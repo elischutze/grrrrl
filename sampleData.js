cb7ba90f427f3c9734bb5dca4359334afe6d696d
tas12 <tsultanah89@gmail.com>
2016-12-08 01:51:15 +0000

    put sample data in js file

---
+++ sampleData.js
@@ -0,0 +1,74 @@
+var sample = {
+  data: [
+    {
+      title: 'ARIA, Accessibility APIs and coding like you give a damn!',
+      speakerName: 'Leonie Watson',
+      embedUrl: 'https://www.youtube.com/embed/qdB8SRhqvFc',
+      description: 'Understanding accessibility mechanics is the key to good interface design. Using ARIA to manipulate the browser’s accessibility tree means you can create interfaces in HTML, SVG and even Web Components that are accessible to assistive technologies, without compromising on functionality or design.',
+      topics: ['Accessibility']
+    },
+    {
+      title: 'Data as Software as Data',
+      speakerName: 'Andrea Burbank',
+      embedUrl: 'https://www.youtube.com/embed/LOMk8TD6l5k',
+      description: 'Andrea gives us an insight into what it’s like to build the data infrastructure of a fast-scaling, popular site such as Pinterest (all the users and food/furniture-porn!), with a delve into the importance of data science when it comes to decisions on product and strategy.',
+      topics: ['Data Science']
+    },
+    {
+      title: 'How To Get an Engineering Team to Eat Their Vegetables',
+      speakerName: 'Duretti Hirpa',
+      embedUrl: 'https://player.vimeo.com/video/172711355',
+      description: 'Duretti is a champion for empathy in engineering. In this talk she focuses on ways engineering teams can foster excellent working environments and cultures with minimally felt process.',
+      topics: ['Team management']
+    },
+    {
+      title: 'How women survivors are taking back the power through technology',
+      speakerName: 'Hera Hussain',
+      embedUrl: 'https://www.youtube.com/embed/iJGAgQzmG3E',
+      description: 'This talk reveals how the internet has the ability to help women find the supportive resources and networks they need to overcome physical and emotional violence.',
+      topics: ['Tech for good']
+    },
+    {
+      title: 'Neural Nets for Newbies',
+      speakerName: 'Melanie Warrick',
+      embedUrl: 'https://www.youtube.com/embed/g-BJSl4zV_g',
+      description: 'Melanie breaks down the neural net structure as simply as possible, so you have a framework on which to grow your knowledge in the space.',
+      topics: ['Neural Networks']
+    },
+    {
+      title: 'Consequences of an Insightful Algorithm',
+      speakerName: 'Carina C. Zona',
+      embedUrl: 'https://www.youtube.com/embed/znwWYR1mzzw',
+      description: 'Carina’s talk has the ability to silence audiences with new perspectives about everyday algorithms',
+      topics: ['UX']
+    },
+    {
+      title: 'Making Badass Developers',
+      speakerName: 'Kathy Sierra',
+      embedUrl: 'https://www.youtube.com/embed/FKTxC9pl-WM',
+      description: 'Kathy has an amazing ability to surprise you with fascinating knowledge about how people learn, grow and get better at certain tasks.',
+      topics: ['Cognitive development']
+    },
+    {
+      title: 'How to Upgrade Democracy for the Internet Era',
+      speakerName: 'Pia Mancini',
+      embedUrl: 'https://www.youtube.com/embed/NXfYNdapq3Q',
+      description: 'Pia Mancini and her colleagues aim to upgrade democracy in Argentina and beyond.',
+      topics: ['Tech for good']
+    },
+    {
+      title: 'Imposter Syndrome and Individual Competence',
+      speakerName: 'Jess-Lynn Rose',
+      embedUrl: 'https://www.youtube.com/embed/taklO4YIMow',
+      description: ' Jessica analyses Imposter Syndrome and exposes the cognitive biases that shape the software industry, and most importantly, what we can do about them.',
+      topics: ['Cognitive development']
+    },
+    {
+      title: 'Operations: A Developer’s Guide',
+      speakerName: 'Anna Shipman',
+      embedUrl: 'https://www.youtube.com/embed/y6hbrS3DheU',
+      description: 'A self-taught developer with a passion for DevOps, Anna gives a great introduction to web ops skills with lots of valuable take-homes.',
+      topics: ['DevOps']
+    },
+  ]
+}
